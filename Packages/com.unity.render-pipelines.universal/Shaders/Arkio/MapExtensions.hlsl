07205f7ac3cf2655b13a59edac7a9643982396ff
Goran Lalic <goran.lalic@toptal.com>
2024-12-11 15:27:33 +0100

    Arkio map shader (it's easier if it's part of our URP fork rather than in Arkio folders)

---
+++ Packages/com.unity.render-pipelines.universal/Shaders/Arkio/MapExtensions.hlsl
@@ -0,0 +1,31 @@
+const half3   _MapSideColor = half3(0.75,0.75,0.75);
+
+int    _ArkioShowMap;
+float4 _ArkioModelSpaceBounds;
+
+half3 ComputeMapColor(Varyings input) 
+{    
+    float2 uv = input.uv;
+    half3 diffuse = _BaseColor.rgb;
+
+    if (_ArkioShowMap > 0.5) {
+        float d = dot(input.normalWS, float3(0, 1, 0));
+        if (d > 0.1) { // top
+            float3 modelPos = (0);
+            modelPos.x = input.positionOS.x;
+            modelPos.z = input.positionOS.z;
+
+            // sample map:
+            float2 samplingPos = (float2(modelPos.z, -modelPos.x) - float2(_ArkioModelSpaceBounds.y, -_ArkioModelSpaceBounds.z));
+            samplingPos.x /= _ArkioModelSpaceBounds.w - _ArkioModelSpaceBounds.y;
+            samplingPos.y /= _ArkioModelSpaceBounds.z - _ArkioModelSpaceBounds.x;
+            diffuse.rgb = SampleAlbedoAlpha(samplingPos, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap));
+        }
+        else { // side is unconditionally equal to _MapSideColor
+            diffuse.rgb = _MapSideColor.rgb;
+        }
+    }
+
+    return diffuse;
+
+}
