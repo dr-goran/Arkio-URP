1bf75cab67d57575bf95f16579da78fb2937fba7
Goran Lalic <goran.lalic@toptal.com>
2025-01-30 09:50:24 +0100

    Tightening the screws on the shader code. Remove ARKIO_SECTION shader feature.

--- Packages/com.unity.render-pipelines.universal/Shaders/Arkio.hlsl
+++ Packages/com.unity.render-pipelines.universal/Shaders/Arkio.hlsl
@@ -5,7 +5,7 @@
 
 #pragma shader_feature _ ARKIO_VEIL
 #pragma shader_feature _ ARKIO_VERTEX_COLORS
-#pragma shader_feature _ ARKIO_SECTION
+
 #pragma shader_feature _ ARKIO_XRAY
 
     #ifdef ARKIO_VEIL
@@ -16,18 +16,6 @@
     uniform float _ArkioGlobalXRayAlphaMultiplier;
     #endif
 
-    #ifdef ARKIO_SECTION
-    uniform float3 _SectionPlanePosition;
-    uniform float3 _SectionPlaneNormal;
     
-    bool sectioned(float3 worldPosition) {
-        return dot(worldPosition - _SectionPlanePosition, _SectionPlaneNormal) 
-        #ifdef ARKIO_SECTION_INVERSE
-            <= 0;
-        #else
-            > 0;
-        #endif
-    }
-    #endif
 
 #endif
---
+++ Packages/com.unity.render-pipelines.universal/Shaders/ArkioSections.hlsl
@@ -0,0 +1,15 @@
+#ifndef ARKIO_SECTIONS_INCLUDED
+#define ARKIO_SECTIONS_INCLUDED
+uniform float3 _SectionPlanePosition;
+uniform float3 _SectionPlaneNormal;
+
+bool sectioned(float3 worldPosition) {
+    return dot(worldPosition - _SectionPlanePosition, _SectionPlaneNormal) Great, 
+    #ifdef ARKIO_SECTION_INVERSE
+        <= 0;
+    #else
+        > 0;
+    #endif
+}
+
+#endif
